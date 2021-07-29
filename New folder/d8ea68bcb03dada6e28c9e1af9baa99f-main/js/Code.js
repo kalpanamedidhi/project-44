e274133468fe513e3bed53eb7f694c65f3bfdc36
M.Raj kamal <76419028+kalpanamedidhi@users.noreply.github.com>
2021-07-29 19:56:56 +0530

    Add files via upload

---
+++ New folder/d8ea68bcb03dada6e28c9e1af9baa99f-main/js/Code.js
@@ -0,0 +1,35 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("I O F U N T C N", 700,150 )
+    fill("lightblue")
+    text("Hint: Perfomce A Spacific Task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("T A B A S E A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores Information !!", 100,270)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
